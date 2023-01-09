a3cb4873698b7314571868d5b54dc8d82df7b377
Ismatou <ismatousadio.souare@edu.univ-paris13.fr>
2023-01-09 22:30:12 +0100

    creation fichier login modification fichier menu avec url_for()

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ app=Flask(__name__)
 #creation de notrefonction home de type route
 
 @app.route('/')
-def home():
+def index():
     return render_template('home.html')
+@app.route('/login')
+def connexion():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
